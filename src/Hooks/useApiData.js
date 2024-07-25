29c78c17270c66f71fd38d33b08a9eedf0b07438
Ayesha Hussain <ayeshakhadim48@gmail.com>
2024-07-25 11:16:37 -0700

    add custom hook for data fetching

---
+++ src/Hooks/useApiData.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "../axios";
+import { toast } from "react-toastify";
+
+export const useApiData = (url, errorMessage = "Failed to fetch data") => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [refetch, setRefetch] = useState(0);
+
+  const getData = async () => {
+    try {
+      const result = await axios.get(url);
+
+      if (result.statusText === "OK") {
+        setData(result.data);
+      } else {
+        toast.error(errorMessage);
+      }
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      setError(err);
+      toast.error(errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [url, refetch]);
+
+  return {
+    data,
+    isLoading,
+    error,
+    refetch: () => setRefetch((r) => r + 1),
+  };
+};
