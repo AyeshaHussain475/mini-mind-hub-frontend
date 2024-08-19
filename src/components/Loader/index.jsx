24d96c32f3bc5e91d4c0a0816358f15c7d680091
Ayesha Hussain <ayeshakhadim48@gmail.com>
2024-08-19 10:34:45 -0700

    added loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div class="loader-container">
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
