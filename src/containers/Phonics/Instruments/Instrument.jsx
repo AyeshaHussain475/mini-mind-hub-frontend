0e528039c9211b7e243f1029b9b0d7ce0907955c
Ayesha Hussain <ayeshakhadim48@gmail.com>
2024-08-26 08:02:37 -0700

    implemented getInstrument endPoint

---
+++ src/containers/Phonics/Instruments/Instrument.jsx
@@ -0,0 +1,109 @@
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import {
+  Delete as DeleteIcon,
+  Edit as EditIcon,
+  PlayArrow as PlayArrowIcon,
+  Pause as PauseIcon,
+} from "@mui/icons-material";
+import { useRef, useState } from "react";
+
+const Instrument = ({ name, image, sound }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleAudio = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        borderRadius: "15px",
+        transition: "transform 0.3s",
+        "&:hover": {
+          transform: "scale(1.05)",
+        },
+      }}
+    >
+      <CardMedia
+        sx={{
+          height: 200,
+          objectFit: "cover",
+          filter: "brightness(90%)",
+        }}
+        component="img"
+        image={image}
+        title={name}
+      />
+      <CardContent
+        sx={{
+          backgroundColor: "#ffcc80",
+        }}
+      >
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            fontFamily: "'Fredoka One', cursive",
+            color: "#4a4a4a",
+            textAlign: "center",
+          }}
+        >
+          {name}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center", gap: "10px" }}>
+        <audio ref={audioRef} src={sound} />
+        <IconButton
+          sx={{
+            backgroundColor: "#fff176",
+            "&:hover": {
+              backgroundColor: "#ffd54f",
+            },
+          }}
+          onClick={handleAudio}
+        >
+          {isPlaying ? (
+            <PauseIcon sx={{ fontSize: 30, color: "#5c6bc0" }} />
+          ) : (
+            <PlayArrowIcon sx={{ fontSize: 30, color: "#5c6bc0" }} />
+          )}
+        </IconButton>
+        <IconButton
+          sx={{
+            backgroundColor: "#ff7043",
+            "&:hover": {
+              backgroundColor: "#ff5722",
+            },
+          }}
+        >
+          <DeleteIcon sx={{ fontSize: 30, color: "white" }} />
+        </IconButton>
+        <IconButton
+          sx={{
+            backgroundColor: "#66bb6a",
+            "&:hover": {
+              backgroundColor: "#43a047",
+            },
+          }}
+        >
+          <EditIcon sx={{ fontSize: 30, color: "white" }} />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Instrument;
---
+++ src/containers/Phonics/Instruments/Instruments.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useApiData } from "../../../hooks/useApiData";
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Instrument from "./Instrument";
+import Loader from "../../../components/Loader";
+import { useNavigate } from "react-router-dom";
+import BackArrow from "../../../assets/arrow.webp";
+
+const Instruments = () => {
+  const [instruments, setInstruments] = useState([]);
+  const instrumentsQuery = useApiData("/instrument/media");
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ backgroundColor: "#f0f8ff", minHeight: "100vh" }}>
+      <div style={{ backgroundColor: "#9d9db8" }}>
+        <Typography
+          style={{
+            textAlign: "center",
+            fontFamily: "'Fredoka One', cursive", // Fun font for kids
+            fontSize: "2.5rem",
+            color: "#FFFFFF", // Bright color
+            textShadow: "2px 2px 4px purple",
+            // margin: "20px 0",
+            animation: "bounce 1s infinite",
+          }}
+        >
+          Come and play with Your own mini Instruments!
+        </Typography>
+      </div>
+      {/* <Loader /> */}
+      {/* Search textfield and Add Button plus Arrow*/}
+
+      <Grid container justifyContent="space-around" mt={2}>
+        <Grid item>
+          <IconButton
+            sx={{
+              "&:hover": {
+                backgroundColor: "#CBC3E3",
+              },
+            }}
+            onClick={() => navigate("/phonics")}
+          >
+            <img src={BackArrow} style={{ width: "80px", height: "80px" }} />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <TextField></TextField>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            style={{ borderRadius: "20px", marginRight: "8px" }}
+            onClick={() => navigate("/phonics/instrument/create")}
+          >
+            Add Instrument
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={5} sx={{ marginTop: "10px" }}>
+        {instrumentsQuery.data?.instruments.map((instrument) => {
+          const image = `http://localhost:7000/mini/media/${instrument.image}`;
+          const sound = `http://localhost:7000/mini/media/${instrument.sound}`;
+          return (
+            <Grid item key={instrument._id}>
+              <Instrument name={instrument.name} image={image} sound={sound} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
+    //play all and all plays
+  );
+};
+
+export default Instruments;
